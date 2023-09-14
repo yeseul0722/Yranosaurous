8efbbdbfde814b615185052d47a27fa32031fa60
신종혁 <shinjh4903@gmail.com>
2023-09-14 17:56:39 +0900

    ✨ 엑스포 메인 페이지 작업 중

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Expo from './pagecompontents/expopage';
 
 function App() {
   return (
     <div className="App">
-      <div> reset </div>
+      <Routes>
+        <Route path="/Expo" element={<Expo />} />
+      </Routes>
     </div>
   );
 }
