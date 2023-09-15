1bd4aceb9ebb59e5810ef912f36a724dde002608
0214kbs <0214kbs@naver.com>
2023-09-15 16:24:35 +0900

    🎨 input box, title 추가 및 디자인

--- admin/src/pagecomponents/mainpage/index.tsx
+++ admin/src/pagecomponents/mainpage/index.tsx
@@ -1,14 +1,25 @@
+import { StyledMainpage, StyledBox, StyledTitle, StyledInputBox, StyledInput } from './Mainpage.styled';
+
 const MainPage = () => {
   return (
-    <div
+    <StyledMainpage
       style={{
-        height: '100vh',
-        width: '100vw',
         backgroundImage: 'url(/images/main/background.png)',
         backgroundPosition: 'center',
         backgroundSize: 'cover',
       }}
-    ></div>
+    >
+      <StyledBox>
+        <StyledTitle>
+          <div>와이라노사우르스</div>
+          <div>관리자 로그인</div>
+        </StyledTitle>
+        <StyledInputBox>
+          <img src="/images/main/key.png" style={{ width: '20px', marginRight: '10px' }} />
+          <StyledInput type="password" placeholder="password" />
+        </StyledInputBox>
+      </StyledBox>
+    </StyledMainpage>
   );
 };
 
