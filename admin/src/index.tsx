b7b383e77d0575ad2c4c3d2548e61fffbf8b06dd
0214kbs <0214kbs@naver.com>
2023-09-15 10:56:23 +0900

    🎨 폰트 추가 및 default theme 설정

--- admin/src/index.tsx
+++ admin/src/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/DefaultTheme';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
 );
 
