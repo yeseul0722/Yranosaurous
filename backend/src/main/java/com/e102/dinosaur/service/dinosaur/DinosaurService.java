4b3f67a2211009ab179b1f707ac438eb5f717266
최연재 <duswo0624@naver.com>
2023-09-18 16:01:18 +0900

    :sparkles: 이름을 활용한 공룡 조회 기능 구현

--- backend/src/main/java/com/e102/dinosaur/service/dinosaur/DinosaurService.java
+++ backend/src/main/java/com/e102/dinosaur/service/dinosaur/DinosaurService.java
@@ -2,10 +2,12 @@ package com.e102.dinosaur.service.dinosaur;
 
 import com.e102.dinosaur.domain.dinosaur.Dinosaur;
 import com.e102.dinosaur.domain.dinosaur.DinosaurRepository;
+import com.e102.dinosaur.domain.dinosaursub.DinosaurSub;
 import com.e102.dinosaur.domain.dinosaursub.DinosaurSubRepository;
 import com.e102.dinosaur.exception.BaseException;
 import com.e102.dinosaur.service.dinosaur.response.DinosaurDetailResponse;
 import com.e102.dinosaur.service.dinosaur.response.DinosaurResponse;
+import com.e102.dinosaur.service.dinosaur.response.DinosaurSubResponse;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
@@ -31,5 +33,14 @@ public class DinosaurService {
                 .orElseThrow(() -> new BaseException("해당 공룡을 찾을 수 없습니다.", 1001));
 
         return DinosaurDetailResponse.from(dinosaur);
+    }
+
+    public List<DinosaurSubResponse> getDinosaurSubsBy(String name) {
+        List<DinosaurSub> dinosaurSubs = dinosaurSubRepository.findAllByScientificNameContains(
+                name);
+
+        return dinosaurSubs.stream()
+                .map(DinosaurSubResponse::from)
+                .collect(Collectors.toList());
     }
 }
