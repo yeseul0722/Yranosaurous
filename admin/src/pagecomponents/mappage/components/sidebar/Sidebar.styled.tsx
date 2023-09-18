84d5e64212a4f271e43d9b217adccc475fe0959f
0214kbs <0214kbs@naver.com>
2023-09-18 12:35:22 +0900

    ✨ 오른쪽 사이드바 추가

---
+++ admin/src/pagecomponents/mappage/components/sidebar/Sidebar.styled.tsx
@@ -0,0 +1,13 @@
+import styled, { css } from 'styled-components';
+
+const StyledSidebar = styled.div`
+  position: absolute;
+  right: 0;
+  top: 0;
+  width: 360px;
+  height: 100%;
+  background-color: rgba(242, 242, 242, 0.8);
+  z-index: 100;
+`;
+
+export { StyledSidebar };
--- admin/src/pagecomponents/mappage/index.tsx
+++ admin/src/pagecomponents/mappage/index.tsx
@@ -1,12 +1,11 @@
 import { Map } from 'react-kakao-maps-sdk';
+import Sidebar from './components/sidebar';
 
 const MapPage = () => {
   return (
-    <div>
-      <Map
-        center={{ lat: 35.056820163386156, lng: 128.39756122959787 }}
-        style={{ width: '100vw', height: '100vh' }}
-      ></Map>
+    <div style={{ position: 'relative', width: '100vw', height: '100vh' }}>
+      <Map center={{ lat: 35.056820163386156, lng: 128.39756122959787 }} style={{ width: '100%', height: '100%' }} />
+      <Sidebar>right sidebar</Sidebar>
     </div>
   );
 };
