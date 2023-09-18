91858631eb38ffd49d8c5abde926cf23926974b3
yeseul0722 <pdy051257@naver.com>
2023-09-19 05:28:21 +0900

    :lipstick: 사이드바 css 업데이트
    전체적인 사이드바 css 업데이트

--- frontend/src/pagecomponents/gosungpage/components/sidebar/index.tsx
+++ frontend/src/pagecomponents/gosungpage/components/sidebar/index.tsx
@@ -1,23 +1,29 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faUtensils, faHouseUser, faSuitcase } from '@fortawesome/free-solid-svg-icons';
+import { StyledCategoryContainer, StyledCircleContainer, StyledSideBarContainer } from './Sidebar.styled';
 
 const GosungSideBar: React.FC = () => {
   return (
-    <div>
-      <div>
-        <FontAwesomeIcon icon={faBars} />
-      </div>
-      <div>
-        <FontAwesomeIcon icon={faUtensils} />
-      </div>
-      <div>
-        <FontAwesomeIcon icon={faHouseUser} />
-      </div>
-      <div>
-        <FontAwesomeIcon icon={faSuitcase} />
-      </div>
-    </div>
+    <StyledSideBarContainer>
+      <StyledCategoryContainer>
+        <StyledCircleContainer>
+          <FontAwesomeIcon icon={faBars} />
+        </StyledCircleContainer>
+      </StyledCategoryContainer>
+      <StyledCategoryContainer>
+        <FontAwesomeIcon icon={faUtensils} style={{ marginBottom: '10px' }} />
+        <div>맛집</div>
+      </StyledCategoryContainer>
+      <StyledCategoryContainer>
+        <FontAwesomeIcon icon={faHouseUser} style={{ marginBottom: '10px' }} />
+        <div>여행</div>
+      </StyledCategoryContainer>
+      <StyledCategoryContainer>
+        <FontAwesomeIcon icon={faSuitcase} style={{ marginBottom: '10px' }} />
+        <div>관광</div>
+      </StyledCategoryContainer>
+    </StyledSideBarContainer>
   );
 };
 
