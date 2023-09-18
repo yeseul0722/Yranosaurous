dd77c96e22807c6928e5e695c9e4a35fdc9fb84e
slbin <smpts00@naver.com>
2023-09-18 15:44:08 +0900

    :sparkles: 맛집 카테코리로 조회 기능 추가

---
+++ backend/src/main/java/com/e102/dinosaur/controller/restaurant/RestaurantController.java
@@ -0,0 +1,29 @@
+package com.e102.dinosaur.controller.restaurant;
+
+
+import com.e102.dinosaur.service.RestaurantService;
+import com.e102.dinosaur.service.response.AbstractRestaurantResponse;
+import com.e102.dinosaur.utils.ApiResponse;
+import com.e102.dinosaur.utils.ApiUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class RestaurantController {
+
+    private final RestaurantService restaurantService;
+
+    @GetMapping("/restaurant/{category}")
+    public ApiResponse<List<AbstractRestaurantResponse>> restaurantsList(@PathVariable int category) {
+        return ApiUtils.success(restaurantService.findRestaurant(category));
+    }
+
+
+}
