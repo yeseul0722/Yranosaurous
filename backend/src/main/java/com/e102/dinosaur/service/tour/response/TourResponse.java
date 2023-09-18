a409d72ba2d4722e6016256a8959cb1fba3ea7c3
최연재 <duswo0624@naver.com>
2023-09-18 16:19:04 +0900

    :sparkles: 전체 관광지 조회

---
+++ backend/src/main/java/com/e102/dinosaur/service/tour/response/TourResponse.java
@@ -0,0 +1,40 @@
+package com.e102.dinosaur.service.tour.response;
+
+import com.e102.dinosaur.domain.tour.Tour;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class TourResponse {
+
+    private Long id;
+    private String name;
+    private String category;
+    private String address;
+    private String content;
+    private String imgUrl;
+
+    @Builder
+    private TourResponse(Long id, String name, String category, String address, String content,
+            String imgUrl) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.address = address;
+        this.content = content;
+        this.imgUrl = imgUrl;
+    }
+
+    public static TourResponse from(Tour tour) {
+        return TourResponse.builder()
+                .id(tour.getId())
+                .name(tour.getName())
+                .category(tour.getCategory())
+                .address(tour.getAddress())
+                .content(tour.getContent())
+                .imgUrl(tour.getImgUrl())
+                .build();
+    }
+}
