233adba18926ab9e0d32a4d17989f019fe17991b
slbin <smpts00@naver.com>
2023-09-18 11:56:16 +0900

    :sparkles: 맛집 , 숙소 엔티티 추가

---
+++ backend/src/main/java/com/e102/dinosaur/domain/menu/MenuRepository.java
@@ -0,0 +1,6 @@
+package com.e102.dinosaur.domain.menu;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MenuRepository extends JpaRepository<Long,Menu> {
+}
---
+++ backend/src/main/java/com/e102/dinosaur/domain/review/Review.java
@@ -0,0 +1,31 @@
+package com.e102.dinosaur.domain.review;
+
+import com.e102.dinosaur.domain.restaurant.Restaurant;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "restraunt_id")
+    private Restaurant restaurant;
+
+    private String title;
+    private String content;
+    private String reviewAddress;
+
+    public Review(String title, String content, String reviewAddress) {
+        this.title = title;
+        this.content = content;
+        this.reviewAddress = reviewAddress;
+    }
+}
