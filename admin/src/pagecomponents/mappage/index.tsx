aa23dd593d9932a2799fc623e676cf7323a29eac
0214kbs <0214kbs@naver.com>
2023-09-18 16:11:16 +0900

    ✨ 클릭한 위치에 마커 표시하고 위경도 받아오기

--- admin/src/pagecomponents/mappage/index.tsx
+++ admin/src/pagecomponents/mappage/index.tsx
@@ -1,14 +1,40 @@
-import { Map } from 'react-kakao-maps-sdk';
+import { Map, MapMarker } from 'react-kakao-maps-sdk';
 import Sidebar from './components/sidebar';
 import Categories from './components/categories';
+import { useState } from 'react';
+type PositionType =
+  | {
+      lat: number;
+      lng: number;
+    }
+  | undefined;
 
 const MapPage = () => {
+  const [position, setPosition] = useState<PositionType>();
   return (
     <div style={{ display: 'flex' }}>
       <Categories></Categories>
       <div style={{ position: 'relative', width: '100vw', height: '100vh' }}>
-        <Map center={{ lat: 35.056820163386156, lng: 128.39756122959787 }} style={{ width: '100%', height: '100%' }} />
-        <Sidebar></Sidebar>
+        <Map
+          center={{ lat: 35.056820163386156, lng: 128.39756122959787 }}
+          style={{ width: '100%', height: '100%' }}
+          onClick={(_t, mouseEvent) =>
+            setPosition({
+              lat: mouseEvent.latLng.getLat(),
+              lng: mouseEvent.latLng.getLng(),
+            })
+          }
+        >
+          {position && <MapMarker position={position} />}
+        </Map>
+        <Sidebar>
+          {position && (
+            <div>
+              <div>위도 : {position.lat}</div>
+              <div>경도 : {position.lng}</div>
+            </div>
+          )}
+        </Sidebar>
       </div>
     </div>
   );
