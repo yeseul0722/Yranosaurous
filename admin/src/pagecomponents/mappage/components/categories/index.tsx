30d76b841261931e3447eca235bb4fdacc274b49
0214kbs <0214kbs@naver.com>
2023-09-18 15:39:00 +0900

    ✨ 카테고리 추가 및 기능 추가 - zustand를 통해 카테고리 상태 관리 - 선택한 카테고리 css 적용

--- admin/src/pagecomponents/mappage/components/categories/index.tsx
+++ admin/src/pagecomponents/mappage/components/categories/index.tsx
@@ -1,7 +1,26 @@
-import { StyledCategories } from './Categories.styled';
+import { useCategoryStore } from '../../../../stores/useCategoryStore';
+import { StyledCategories, StyledCategory } from './Categories.styled';
 
-const Categories = (props: any) => {
-  return <StyledCategories {...props}>{props.children}</StyledCategories>;
+const Categories = () => {
+  const { selectcat, setselectcat } = useCategoryStore();
+  return (
+    <StyledCategories>
+      <StyledCategory selected={selectcat === '1'} onClick={() => setselectcat('1')}>
+        <img src="/images/map/position.png" style={{ width: '35px' }} />
+        <div>
+          <div>장소</div>
+          <div>등록</div>
+        </div>
+      </StyledCategory>
+      <StyledCategory selected={selectcat === '2'} onClick={() => setselectcat('2')}>
+        <img src="/images/map/course.png" style={{ width: '40px' }} />
+        <div>
+          <div>코스</div>
+          <div>등록</div>
+        </div>
+      </StyledCategory>
+    </StyledCategories>
+  );
 };
 
 export default Categories;
--- admin/src/pagecomponents/mappage/index.tsx
+++ admin/src/pagecomponents/mappage/index.tsx
@@ -5,10 +5,10 @@ import Categories from './components/categories';
 const MapPage = () => {
   return (
     <div style={{ display: 'flex' }}>
-      <Categories> category</Categories>
+      <Categories></Categories>
       <div style={{ position: 'relative', width: '100vw', height: '100vh' }}>
         <Map center={{ lat: 35.056820163386156, lng: 128.39756122959787 }} style={{ width: '100%', height: '100%' }} />
-        <Sidebar>right sidebar</Sidebar>
+        <Sidebar></Sidebar>
       </div>
     </div>
   );
