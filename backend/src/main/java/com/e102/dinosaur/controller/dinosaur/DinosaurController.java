69eb71110c40fb4646df5b4851390483a08c28b2
최연재 <duswo0624@naver.com>
2023-09-18 15:59:24 +0900

    :sparkles: 공룡 분류 조회 기능 구현

--- backend/src/main/java/com/e102/dinosaur/controller/dinosaur/DinosaurController.java
+++ backend/src/main/java/com/e102/dinosaur/controller/dinosaur/DinosaurController.java
@@ -1,6 +1,10 @@
 package com.e102.dinosaur.controller.dinosaur;
 
-import com.e102.dinosaur.domain.dinosaur.DinosaurRepository;
+import com.e102.dinosaur.service.dinosaur.DinosaurService;
+import com.e102.dinosaur.service.dinosaur.response.DinosaurResponse;
+import com.e102.dinosaur.utils.ApiResponse;
+import com.e102.dinosaur.utils.ApiUtils;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,11 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class DinosaurController {
 
-    private final DinosaurRepository dinosaurRepository;
+    private final DinosaurService dinosaurService;
 
     @GetMapping("/dinosaurs")
-    public int authenticationPayment() {
-        return dinosaurRepository.findAll().size();
+    public ApiResponse<List<DinosaurResponse>> getDinosaurs() {
+        return ApiUtils.success(dinosaurService.getDinosaurs());
     }
-
 }
