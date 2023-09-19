168707f2b27d8acb28d8fc6806580d9e48973e05
0214kbs <0214kbs@naver.com>
2023-09-19 09:28:01 +0900

    ✨ 관리자 장소등록 입력폼 생성

--- admin/src/pagecomponents/mappage/components/sidebar/index.tsx
+++ admin/src/pagecomponents/mappage/components/sidebar/index.tsx
@@ -9,9 +9,32 @@ const Sidebar = (props: any) => {
       {selectcat === '2' && <StyledTitle>코스 등록하기</StyledTitle>}
       {props.position && (
         <div>
-          <StyledSubTitle>현재 위치</StyledSubTitle>
-          <div>위도 : {props.position.lat}</div>
-          <div>경도 : {props.position.lng}</div>
+          <div>
+            <StyledSubTitle>현재 위치</StyledSubTitle>
+            <div>위도 : {props.position.lat}</div>
+            <div>경도 : {props.position.lng}</div>
+          </div>
+          <div>
+            <StyledSubTitle>장소 TYPE</StyledSubTitle>
+            <div style={{ display: 'flex' }}>
+              <button>편의 시설</button>
+              <button>관람 요소</button>
+            </div>
+            <div>
+              <StyledSubTitle>장소명</StyledSubTitle>
+              <input />
+            </div>
+            <div>
+              <StyledSubTitle>이미지</StyledSubTitle>
+              <input type="file" />
+            </div>
+            <div>
+              <StyledSubTitle>세부사항</StyledSubTitle>
+              <input />
+            </div>
+          </div>
+          <button>저장하기</button>
+          <button>삭제하기</button>
         </div>
       )}
     </StyledSidebar>
