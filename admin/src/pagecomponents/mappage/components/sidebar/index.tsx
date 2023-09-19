3d3e56150b06f4a8ff2cb4167dbca68bb8184f07
0214kbs <0214kbs@naver.com>
2023-09-19 09:36:37 +0900

    ✨ 입력폼 저장 기능 추가(콘솔창 출력 대체)

--- admin/src/pagecomponents/mappage/components/sidebar/index.tsx
+++ admin/src/pagecomponents/mappage/components/sidebar/index.tsx
@@ -1,8 +1,24 @@
+import { useState } from 'react';
 import { useCategoryStore } from '../../../../stores/useCategoryStore';
 import { StyledSidebar, StyledSubTitle, StyledTitle } from './Sidebar.styled';
 
 const Sidebar = (props: any) => {
   const { selectcat } = useCategoryStore();
+  const [placeName, setPlaceName] = useState('');
+  const [placeType, setPlaceType] = useState('');
+  const [image, setImage] = useState<File | null>(null);
+
+  const handleSaveClick = () => {
+    const data = {
+      위도: props.position.lat,
+      경도: props.position.lng,
+      장소TYPE: placeType,
+      장소명: placeName,
+      이미지: image ? image.name : '없음',
+    };
+    console.log(JSON.stringify(data, null, 2));
+  };
+
   return (
     <StyledSidebar {...props}>
       {selectcat === '1' && <StyledTitle>장소 등록하기</StyledTitle>}
@@ -17,23 +33,23 @@ const Sidebar = (props: any) => {
           <div>
             <StyledSubTitle>장소 TYPE</StyledSubTitle>
             <div style={{ display: 'flex' }}>
-              <button>편의 시설</button>
-              <button>관람 요소</button>
+              <button onClick={() => setPlaceType('편의 시설')}>편의 시설</button>
+              <button onClick={() => setPlaceType('관람 요소')}>관람 요소</button>
             </div>
             <div>
               <StyledSubTitle>장소명</StyledSubTitle>
-              <input />
+              <input value={placeName} onChange={(e) => setPlaceName(e.target.value)} />
             </div>
             <div>
               <StyledSubTitle>이미지</StyledSubTitle>
-              <input type="file" />
+              <input type="file" onChange={(e) => setImage(e.target.files ? e.target.files[0] : null)} />
             </div>
             <div>
               <StyledSubTitle>세부사항</StyledSubTitle>
-              <input />
             </div>
           </div>
-          <button>저장하기</button>
+          <button onClick={handleSaveClick}>저장하기</button>
+
           <button>삭제하기</button>
         </div>
       )}
