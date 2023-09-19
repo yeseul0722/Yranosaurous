1b289b8c5cb554484bc52873de895cf549dca819
최연재 <duswo0624@naver.com>
2023-09-19 09:30:51 +0900

    :sparkles: ControllerAdvice 추가

---
+++ backend/src/main/java/com/e102/dinosaur/utils/GlobalExceptionHandler.java
@@ -0,0 +1,47 @@
+package com.e102.dinosaur.utils;
+
+import com.e102.dinosaur.exception.BaseException;
+import com.e102.dinosaur.exception.ValidationException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BaseException.class)
+    @ResponseBody
+    public ApiResponse<?> handleAllExceptions(BaseException ex) {
+        return ApiUtils.error(ex.getApiError());
+    }
+
+    @ExceptionHandler(ValidationException.class)
+    @ResponseBody
+    public ApiResponse<?> handleAllExceptions(ValidationException ex) {
+        for (FieldError fieldError : ex.getFieldErrors()) {
+            String fieldName = fieldError.getField();
+            String errorMessage = fieldError.getDefaultMessage();
+            Object rejectedValue = fieldError.getRejectedValue();
+            return ApiUtils.error(errorMessage, 1000);
+        }
+        return ApiUtils.error("Valid 에러", 1000);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ApiResponse<?> methodArgumentNotValidException(MethodArgumentNotValidException e) {
+        return ApiUtils.error(
+                e.getBindingResult().getAllErrors().get(0).getDefaultMessage(),
+                1000
+        );
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ApiResponse<?> illegalArgumentException(IllegalArgumentException e) {
+        return ApiUtils.error(
+                e.getMessage(),
+                1000
+        );
+    }
+}
