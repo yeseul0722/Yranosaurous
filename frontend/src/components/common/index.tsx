6097f123c37d7d63b28f759aeb124ef30ec7d589
신종혁 <shinjh4903@gmail.com>
2023-09-20 15:34:49 +0900

    💄 NavBar 레이아웃 구현

---
+++ frontend/src/components/common/index.tsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import {
+  StyledNavBar,
+  StyledNavBarDropDown,
+  StyledLogoContainer,
+  StyledLogo,
+  StyledName,
+  StyledEndComponent,
+  StyledLinkText,
+  StyledLinkDetailContainer,
+  StyledLinkDetail,
+} from './Navbar.styled';
+
+const NavBarComponent = () => {
+  const [navState, setNavState] = useState('');
+  const onMouseEnter = (e: any) => {
+    setNavState(e.target.name);
+  };
+  const onMouseLeave = (e: any) => {
+    setNavState('');
+  };
+
+  const onClick = (e: any) => {
+    e.preventDefault();
+    setNavState(e.target.name);
+  };
+  return (
+    <StyledNavBar>
+      <StyledLogoContainer>
+        <StyledLogo src="/Logo.png" alt="Logo" />
+        <StyledName>와이라노사우르스</StyledName>
+      </StyledLogoContainer>
+      <StyledEndComponent>
+        <StyledLinkText name="expo" onMouseEnter={onMouseEnter}>
+          Expo
+        </StyledLinkText>
+        <StyledLinkText name="tour" onMouseEnter={onMouseEnter}>
+          고성관광
+        </StyledLinkText>
+        <StyledLinkText name="dino" onMouseEnter={onMouseEnter}>
+          공룡
+        </StyledLinkText>
+      </StyledEndComponent>
+      {navState === 'expo' && (
+        <StyledNavBarDropDown onMouseLeave={onMouseLeave}>
+          <StyledLinkDetailContainer>
+            <StyledLinkDetail>행사장 안내</StyledLinkDetail>
+            <StyledLinkDetail>공연 정보</StyledLinkDetail>
+          </StyledLinkDetailContainer>
+        </StyledNavBarDropDown>
+      )}
+      {navState === 'tour' && (
+        <StyledNavBarDropDown onMouseLeave={onMouseLeave}>
+          <StyledLinkDetailContainer>
+            <StyledLinkDetail></StyledLinkDetail>
+            <StyledLinkDetail></StyledLinkDetail>
+          </StyledLinkDetailContainer>
+        </StyledNavBarDropDown>
+      )}
+      {navState === 'dino' && (
+        <StyledNavBarDropDown onMouseLeave={onMouseLeave}>
+          <StyledLinkDetailContainer>
+            <StyledLinkDetail></StyledLinkDetail>
+            <StyledLinkDetail></StyledLinkDetail>
+          </StyledLinkDetailContainer>
+        </StyledNavBarDropDown>
+      )}
+    </StyledNavBar>
+  );
+};
+
+export default NavBarComponent;
