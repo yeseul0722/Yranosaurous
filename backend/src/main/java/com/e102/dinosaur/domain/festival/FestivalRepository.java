a5edfd96438310e03adb95d1fecfc53a0bf0b59a
slbin <smpts00@naver.com>
2023-09-21 16:38:38 +0900

    :sparkles: 장소 ,코스 도메인 추가

---
+++ backend/src/main/java/com/e102/dinosaur/domain/festival/FestivalRepository.java
@@ -0,0 +1,6 @@
+package com.e102.dinosaur.domain.festival;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FestivalRepository extends JpaRepository<Festival, Long> {
+}
---
+++ backend/src/main/java/com/e102/dinosaur/domain/place/PlaceRepository.java
@@ -0,0 +1,6 @@
+package com.e102.dinosaur.domain.place;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlaceRepository extends JpaRepository<Place, Long> {
+}
