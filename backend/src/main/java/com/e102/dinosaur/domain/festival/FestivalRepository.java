f5bbfd61a7b5177765c75dd930b4193ee76ef223
slbin <smpts00@naver.com>
2023-09-22 11:38:47 +0900

    :sparkles: 날짜 기준 공연 조회 api 추가

--- backend/src/main/java/com/e102/dinosaur/domain/festival/FestivalRepository.java
+++ backend/src/main/java/com/e102/dinosaur/domain/festival/FestivalRepository.java
@@ -2,5 +2,10 @@ package com.e102.dinosaur.domain.festival;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface FestivalRepository extends JpaRepository<Festival, Long> {
+
+    List<Festival> findByStartTimeBetween(LocalDateTime start, LocalDateTime end);
 }
