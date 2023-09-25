7998e6bede937da99524542ca9c76a0899505394
yeseul0722 <pdy051257@naver.com>
2023-09-26 03:57:21 +0900

    :lipstick: 음식점 디테일 페이지 css
    음식점 디테일 페이지 css

--- frontend/src/pagecomponents/gosungpage/components/kakaomap/index.tsx
+++ frontend/src/pagecomponents/gosungpage/components/kakaomap/index.tsx
@@ -1,10 +1,47 @@
-import React from 'react';
-import { Map } from 'react-kakao-maps-sdk';
+import React, { useState, useEffect } from 'react';
+import { Map, MapMarker } from 'react-kakao-maps-sdk';
 import { StyledKakaoMapContainer } from './KakaoMap.styled';
 import { useRestaurantStore } from '../../../../stores/restaurants/useRestaurantApiStore';
 
-const GosungKakaoMapComponent = () => {
+interface GeocoderResult {
+  y: any;
+  x: any;
+}
+
+const GosungKakaoMapComponent: React.FC = () => {
   const { restaurantList } = useRestaurantStore();
+  const [markers, setMarkers] = useState<JSX.Element[]>([]);
+
+  console.log(markers, 'markers');
+
+  useEffect(() => {
+    const geocoder = new kakao.maps.services.Geocoder();
+
+    restaurantList.forEach((restaurant) => {
+      geocoder.addressSearch(
+        restaurant.address,
+        function (result: GeocoderResult[], status: kakao.maps.services.Status) {
+          // 정상적으로 검색이 완료됐으면
+          if (status === kakao.maps.services.Status.OK && result[0]) {
+            const coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+
+            // 결과값으로 받은 위치를 마커로 표시합니다
+            setMarkers((prev) => [
+              ...prev,
+              <MapMarker
+                key={`${coords.getLat()}-${coords.getLng()}`}
+                position={{
+                  lat: coords.getLat(),
+                  lng: coords.getLng(),
+                }}
+                clickable={true}
+              />,
+            ]);
+          }
+        },
+      );
+    });
+  }, [restaurantList]);
 
   return (
     <Map
@@ -18,7 +55,9 @@ const GosungKakaoMapComponent = () => {
         height: '100vh',
       }}
       level={3}
-    />
+    >
+      {markers}
+    </Map>
   );
 };
 
