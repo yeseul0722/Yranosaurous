ef1c49c10846fbae2556e0235fea67c0510d2796
0214kbs <0214kbs@naver.com>
2023-09-26 15:45:14 +0900

    ✨ 공연 목록 보여주기

--- admin/src/pagecomponents/mappage/components/enrollshow/index.tsx
+++ admin/src/pagecomponents/mappage/components/enrollshow/index.tsx
@@ -2,14 +2,25 @@ import { EnrollShowPropsType } from '../../Map.type';
 import { StyledBox, StyledSubTitle } from './EnrollShow.styled';
 
 const EnrollShow = ({ place, festivals }: EnrollShowPropsType) => {
-  console.log('festival', festivals);
   return (
-    <div>
+    <div style={{ display: 'flex', flexDirection: 'column', gap: '18px' }}>
       {place && (
         <div>
           <StyledSubTitle>장소 이름</StyledSubTitle>
           <StyledBox>
             <div style={{ paddingLeft: '15px' }}>{place.name}</div>
+          </StyledBox>
+        </div>
+      )}
+      {festivals && festivals.length > 0 && (
+        <div>
+          <StyledSubTitle>공연 목록</StyledSubTitle>
+          <StyledBox>
+            {festivals.map((festival) => (
+              <div style={{ paddingLeft: '15px' }} key={festival.id}>
+                {festival.name}
+              </div>
+            ))}
           </StyledBox>
         </div>
       )}
