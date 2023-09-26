cfb0b96169576e909f998876f565fd3ed77599fd
0214kbs <0214kbs@naver.com>
2023-09-26 12:42:32 +0900

    ✨ 공연 정보 마커 클릭시 보여주는 화면

---
+++ admin/src/pagecomponents/mappage/components/enrollshow/EnrollShow.type.ts
@@ -0,0 +1,12 @@
+export interface EnrollShowPropsType {
+  place: {
+    id: string;
+    name: string;
+    latitude: string;
+    longitude: string;
+    markerNumber: string;
+    type: string;
+    content: string;
+    imgAddress: string;
+  };
+}
