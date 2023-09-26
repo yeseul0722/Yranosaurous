27fda8abab87ef1ee3cf19c3403c7db6be805915
slbin <smpts00@naver.com>
2023-09-26 12:24:46 +0900

    :sparkles: 배포 설정 추가

---
+++ backend/deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source .deploy.env
+
+echo "> gradle 빌드  "
+./gradlew build
+
+echo "> 도커 빌드"
+docker build -t slbin/dino-dev .
+
+echo "> 도커 로그인"
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+echo "> 도커 허브 푸시"
+docker push slbin/dino-dev
+
+echo -e "\033[32m > AWS docker-compose shell 실행 \033[0m"
+ssh -i $AWS_PEM $AWS_USER@$AWS_IP << EOF
+    echo "디렉토리 이동"
+    cd /home/ubuntu/spring
+    echo "쉘 실행"
+    $SHELL_RUN
+EOF
