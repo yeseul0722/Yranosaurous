fa9450a9b9e948493de12654298a6d8ba80941a1
0214kbs <0214kbs@naver.com>
2023-09-26 17:41:12 +0900

    ✨ 공연정보 표시

--- admin/src/pagecomponents/mappage/components/enrollshow/index.tsx
+++ admin/src/pagecomponents/mappage/components/enrollshow/index.tsx
@@ -1,7 +1,20 @@
-import { EnrollShowPropsType } from '../../Map.type';
-import { StyledBox, StyledSubTitle } from './EnrollShow.styled';
+import { useState } from 'react';
+import Button from '../../../../components/button';
+import { EnrollShowPropsType, Festival } from '../../Map.type';
+import { StyledBox, StyledShowTitle, StyledSubTitle } from './EnrollShow.styled';
 
 const EnrollShow = ({ place, festivals }: EnrollShowPropsType) => {
+  const [buttontype, setButtontype] = useState('add');
+  const [selectedFestival, setSelectedFestival] = useState<Festival | null>(null);
+
+  const toggleButtonType = () => {
+    setSelectedFestival(null);
+    setButtontype(buttontype === 'add' ? 'backtolist' : 'add');
+  };
+  const handleFestivalClick = (festival: Festival) => {
+    setButtontype('backtolist');
+    setSelectedFestival(festival);
+  };
   return (
     <div style={{ display: 'flex', flexDirection: 'column', gap: '18px' }}>
       {place && (
@@ -12,16 +25,42 @@ const EnrollShow = ({ place, festivals }: EnrollShowPropsType) => {
           </StyledBox>
         </div>
       )}
-      {festivals && festivals.length > 0 && (
+      {buttontype === 'add' && festivals && festivals.length > 0 && (
         <div>
           <StyledSubTitle>공연 목록</StyledSubTitle>
           <StyledBox>
             {festivals.map((festival) => (
-              <div style={{ paddingLeft: '15px' }} key={festival.id}>
+              <div
+                style={{ paddingLeft: '15px', cursor: 'pointer' }}
+                key={festival.id}
+                onClick={() => handleFestivalClick(festival)}
+              >
                 {festival.name}
               </div>
             ))}
           </StyledBox>
+        </div>
+      )}
+      {place && (
+        <div style={{ height: '35px' }}>
+          <Button
+            ismain="true"
+            label={buttontype === 'add' ? '공연 추가하기' : '리스트로 돌아가기'}
+            onClick={toggleButtonType}
+          />
+        </div>
+      )}
+      {selectedFestival && (
+        <div>
+          <StyledSubTitle>공연 정보</StyledSubTitle>
+          <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+            <StyledShowTitle>{selectedFestival.name}</StyledShowTitle>
+            <div>{selectedFestival.startTime.split('T')[0].split('-').join('.')}</div>
+            <div>{selectedFestival.startTime.split('T')[1].substring(0, 5)}</div>
+            <div style={{ height: '25px' }}>
+              <Button label="삭제" />
+            </div>
+          </div>
         </div>
       )}
     </div>
