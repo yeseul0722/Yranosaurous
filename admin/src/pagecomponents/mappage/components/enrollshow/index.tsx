e347baceb81121ab88b1fba25df7589f9e7bc8de
0214kbs <0214kbs@naver.com>
2023-09-27 14:49:46 +0900

    ✨ 공연정보 저장 api 연동

--- admin/src/pagecomponents/mappage/components/enrollshow/index.tsx
+++ admin/src/pagecomponents/mappage/components/enrollshow/index.tsx
@@ -1,22 +1,36 @@
 import { useState } from 'react';
 import Button from '../../../../components/button';
 import { EnrollShowPropsType } from '../../Map.type';
-import { StyledBox, StyledShowText, StyledShowTitle, StyledSubTitle } from './EnrollShow.styled';
+import { StyledBox, StyledFestivalList, StyledSubTitle } from './EnrollShow.styled';
 import Input from '../../../../components/input';
 import FestivalInfo from '../festivalinfo';
+import enrollShowPost from '../../../../apis/show/enrollShowPost';
 
 const EnrollShow = ({ place, festivals }: EnrollShowPropsType) => {
-  const [inputs, setInputs] = useState<{ name: string; date: string; time: string }[]>([]);
+  const [input, setInput] = useState<{ name: string; date: string; time: string } | null>(null);
 
   const handleAddClick = () => {
-    setInputs([...inputs, { name: '', date: '', time: '' }]);
+    setInput({ name: '', date: '', time: '' });
   };
-  const handleChange = (index: number, field: string, value: string) => {
-    setInputs((prevInputs) => prevInputs.map((input, i) => (i === index ? { ...input, [field]: value } : input)));
+  const handleSaveClick = async () => {
+    // console.log(`${input?.date} ${input?.time}`);
+    try {
+      const data = {
+        name: input?.name,
+        startTime: `${input?.date} ${input?.time}`,
+        placeId: place?.id,
+      };
+      const response = await enrollShowPost(data);
+      if (response) {
+        // console.log('Successfully posted:', response);
+      }
+    } catch (err) {
+      // console.error('Error posting data:', err);
+    }
   };
 
-  const handleDeleteClick = (index: number) => {
-    setInputs((prevInputs) => prevInputs.filter((_, i) => i !== index));
+  const handleChange = (field: string, value: string) => {
+    if (input) setInput({ ...input, [field]: value });
   };
 
   return (
@@ -31,7 +45,7 @@ const EnrollShow = ({ place, festivals }: EnrollShowPropsType) => {
           </div>
           {!place && <StyledSubTitle> 등록된 장소를 클릭해주세요!</StyledSubTitle>}
           {festivals && festivals.length > 0 && (
-            <div>
+            <StyledFestivalList>
               <StyledSubTitle>공연 목록</StyledSubTitle>
               <div>
                 {festivals.map((festival) => (
@@ -40,16 +54,15 @@ const EnrollShow = ({ place, festivals }: EnrollShowPropsType) => {
                   </div>
                 ))}
               </div>
-            </div>
+            </StyledFestivalList>
           )}
           {place && (
             <div style={{ height: '35px' }}>
               <Button ismain="true" label={'공연 추가하기'} onClick={handleAddClick} />
             </div>
           )}
-          {inputs.map((input, index) => (
+          {input && (
             <div
-              key={index}
               style={{
                 display: 'flex',
                 flexDirection: 'column',
@@ -63,7 +76,7 @@ const EnrollShow = ({ place, festivals }: EnrollShowPropsType) => {
                     type="text"
                     placeholder="공연 이름"
                     value={input.name}
-                    onChange={(e: any) => handleChange(index, 'name', e.target.value)}
+                    onChange={(e: any) => handleChange('name', e.target.value)}
                   />
                 </div>
                 <div style={{ display: 'flex', gap: '3px' }}>
@@ -72,32 +85,24 @@ const EnrollShow = ({ place, festivals }: EnrollShowPropsType) => {
                       ismain="false"
                       label={'삭제'}
                       style={{ border: '1.6px solid #599198' }}
-                      onClick={() => handleDeleteClick(index)}
+                      onClick={() => setInput(null)} // input 객체 삭제
                     />
                   </div>
                   <div style={{ height: '39px', width: '45px' }}>
-                    <Button ismain="true" label={'저장'} />
+                    <Button ismain="true" label={'저장'} onClick={handleSaveClick} />
                   </div>
                 </div>
               </div>
               <div style={{ display: 'flex', gap: '11px' }}>
                 <div style={{ width: '47%', height: '35px' }}>
-                  <Input
-                    type="date"
-                    value={input.date}
-                    onChange={(e: any) => handleChange(index, 'date', e.target.value)}
-                  />
+                  <Input type="date" value={input.date} onChange={(e: any) => handleChange('date', e.target.value)} />
                 </div>
                 <div style={{ width: '47%', height: '35px' }}>
-                  <Input
-                    type="time"
-                    value={input.time}
-                    onChange={(e: any) => handleChange(index, 'time', e.target.value)}
-                  />
+                  <Input type="time" value={input.time} onChange={(e: any) => handleChange('time', e.target.value)} />
                 </div>
               </div>
             </div>
-          ))}
+          )}
         </div>
       </div>
     </div>
