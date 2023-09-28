5c165eced5485f150733fd466d6ab3a6edc50101
신종혁 <shinjh4903@gmail.com>
2023-09-29 01:10:44 +0900

    ✨ 고성 관광 모바일 별점 커스텀 기능 구현

--- frontend/src/mobilepagecomponents/gosungmobile/components/restaurantmodal/Modal.styled.tsx
+++ frontend/src/mobilepagecomponents/gosungmobile/components/restaurantmodal/Modal.styled.tsx
@@ -37,7 +37,7 @@ const ModalContainer = styled.div`
 
 const ModalTopContainer = styled.div.attrs<any>((props) => ({}))`
   max-width: 100%;
-  height: 15%;
+  height: 25%;
   display: flex;
   flex-direction: column;
 `;
