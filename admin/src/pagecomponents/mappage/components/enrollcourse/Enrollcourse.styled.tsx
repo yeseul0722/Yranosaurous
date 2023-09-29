035d0929d9d73847f349aa0ee443ee505cce0889
0214kbs <0214kbs@naver.com>
2023-09-29 10:51:52 +0900

    ✨ 코스 부분 정보 보여주기

--- admin/src/pagecomponents/mappage/components/enrollcourse/Enrollcourse.styled.tsx
+++ admin/src/pagecomponents/mappage/components/enrollcourse/Enrollcourse.styled.tsx
@@ -1,5 +1,13 @@
 import styled, { css } from 'styled-components';
 
+const StyledSidebar = styled.div`
+  gap: 20px;
+  height: 77vh;
+  overflow: hidden auto;
+  padding-right: 7px;
+  display: flex;
+  flex-direction: column;
+`;
 const StyledCourseMap = styled.div.attrs<any>((props) => ({}))`
   ${(props) => {
     const font = props.theme.fonts.boldfont;
@@ -15,4 +23,36 @@ const StyledCourseMap = styled.div.attrs<any>((props) => ({}))`
   }}
 `;
 
-export { StyledCourseMap };
+const StyledSubTitle = styled.div.attrs<any>((props) => ({}))`
+  ${(props) => {
+    const font = props.theme.fonts.boldfont;
+
+    return css`
+      font-family: ${font};
+      font-size: 18px;
+      margin-bottom: 10px;
+    `;
+  }}
+`;
+const StyledBox = styled.div.attrs<any>((props) => ({}))`
+  ${(props) => {
+    const font = props.theme.fonts.regularfont;
+    const black = props.theme.colors.black;
+    const main = props.theme.colors.main;
+    const white = props.theme.colors.white;
+
+    return css`
+      font-family: ${font};
+      font-size: 15px;
+      line-height: 35px;
+      height: 35px;
+      width: 100%;
+      color: ${black};
+      background-color: ${white};
+      border: 1.7px solid ${main};
+      margin-bottom: 5px;
+    `;
+  }}
+`;
+
+export { StyledSidebar, StyledCourseMap, StyledSubTitle, StyledBox };
