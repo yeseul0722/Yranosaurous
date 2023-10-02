572951e742f9bc67d4b3123b0d037750491883e4
slbin-park <smpts00@naver.com>
2023-10-02 22:02:25 +0900

    :sparkles: 맛집 api 정보 수정

--- backend/src/main/java/com/e102/dinosaur/service/place/response/FestivalResponse.java
+++ backend/src/main/java/com/e102/dinosaur/service/place/response/FestivalResponse.java
@@ -1,6 +1,7 @@
 package com.e102.dinosaur.service.place.response;
 
 import com.e102.dinosaur.domain.festival.Festival;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -18,6 +19,8 @@ public class FestivalResponse {
     private String name;
     private LocalDateTime startTime;
     private String placeName;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public PlaceResponse placeResponse;
 
     public static FestivalResponse of(Festival festival) {
         return FestivalResponse.builder()
@@ -25,6 +28,16 @@ public class FestivalResponse {
                 .name(festival.getName())
                 .startTime(festival.getStartTime())
                 .placeName(festival.getPlace().getName())
+                .build();
+    }
+
+    public static FestivalResponse placeOf(Festival festival) {
+        return FestivalResponse.builder()
+                .id(festival.getId())
+                .name(festival.getName())
+                .startTime(festival.getStartTime())
+                .placeName(festival.getPlace().getName())
+                .placeResponse(PlaceResponse.of(festival.getPlace()))
                 .build();
     }
 
