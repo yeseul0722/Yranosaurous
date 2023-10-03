99b56ea4e790f495c2fcee86d0f39f1be2619a3c
김예슬 <pdy051257@naver.com>
2023-10-03 18:32:46 +0900

    :sparkles: 카카오맵 커스텀 오버레이 구현
    카카오맵 커스텀 오버레이 구현

--- frontend/src/pagecomponents/gosungpage/components/kakaomap/KakaoMap.styled.tsx
+++ frontend/src/pagecomponents/gosungpage/components/kakaomap/KakaoMap.styled.tsx
@@ -4,4 +4,15 @@ const StyledKakaoMapContainer = styled.div`
   position: relative;
 `;
 
-export { StyledKakaoMapContainer };
+const StyledMarkerContainer = styled.div``;
+
+const StyledInfoWindow = styled.div`
+  padding: 5px;
+  border: 3px solid ${(props) => props.theme.colors.main};
+  background-color: ${(props) => props.theme.colors.white};
+  border-radius: 10px;
+  font-size: 13px;
+  font-family: ${(props) => props.theme.fonts.regularfont};
+`;
+
+export { StyledKakaoMapContainer, StyledInfoWindow, StyledMarkerContainer };
