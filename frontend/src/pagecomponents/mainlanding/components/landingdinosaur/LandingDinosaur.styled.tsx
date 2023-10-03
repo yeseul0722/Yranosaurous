17e468686304d3721388e7cb7a3f5dc68cec7d9f
alsuddlek1 <minyoung99311@gmail.com>
2023-10-04 03:09:45 +0900

    :sparkles: Landing 셋팅
    landing 셋팅

---
+++ frontend/src/pagecomponents/mainlanding/components/landingdinosaur/LandingDinosaur.styled.tsx
@@ -0,0 +1,62 @@
+import styled from 'styled-components';
+
+const StyledLandingDinosaurPage = styled.div`
+  height: 100vh;
+  /* background-color: ${(props) => props.theme.colors.night}; */
+  background-image: url(//unpkg.com/three-globe/example/img/night-sky.png);
+  object-fit: cover;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  position: relative;
+`;
+
+const StyledLandingDinosaurContent = styled.div`
+  color: ${(props) => props.theme.colors.white};
+  position: absolute;
+  top: 150px;
+  font-family: ${(props) => props.theme.fonts.dinoboldfont};
+  line-height: 150%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  cursor: default;
+`;
+
+const StyledLandingDinosaurLogo = styled.img.attrs<any>((props) => ({
+  src: props.src,
+  alt: props.alt,
+}))`
+  width: 40rem;
+  margin-bottom: 1rem;
+`;
+
+const StyledLandingDinosaurText = styled.div`
+  cursor: default;
+  font-size: 24px;
+`;
+
+const StyledLandingDinosaurLinkButton = styled.div`
+  border-radius: 50px;
+  background-color: ${(props) => props.theme.colors.main};
+  width: 15rem;
+  height: 4rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 24px;
+  margin-top: 2rem;
+`;
+
+const StyledLandingGlobe = styled.div`
+  padding-top: 28rem;
+`;
+
+export {
+  StyledLandingDinosaurPage,
+  StyledLandingDinosaurContent,
+  StyledLandingDinosaurLogo,
+  StyledLandingDinosaurLinkButton,
+  StyledLandingGlobe,
+  StyledLandingDinosaurText,
+};
---
+++ frontend/src/pagecomponents/mainlanding/components/landingstart/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyledLandingStartPage, StyledLandingStartPageLogo } from './LandingStart.styeld';
+
+const LandingStart = () => {
+  return (
+    <StyledLandingStartPage>
+      <StyledLandingStartPageLogo src="/landing/mainlogo.png" alt="landinglogo" />
+    </StyledLandingStartPage>
+  );
+};
+
+export default LandingStart;
