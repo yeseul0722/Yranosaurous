6db92119356652e224fbe8750dd83920aad11751
김예슬 <pdy051257@naver.com>
2023-10-04 13:56:38 +0900

    :lipstick: 맛집 디테일 디자인 변경
    맛집 디테일 디자인 변경

--- frontend/src/pagecomponents/gosungpage/components/restaurantreview/RestaurantReview.styled.tsx
+++ frontend/src/pagecomponents/gosungpage/components/restaurantreview/RestaurantReview.styled.tsx
@@ -6,6 +6,8 @@ const StyledReviewContainer = styled.div`
   margin-top: 2px;
   overflow: auto;
   border-radius: 5px;
+  background-color: ${(props) => props.theme.colors.light};
+  border: 5px solid ${(props) => props.theme.colors.main};
   &::-webkit-scrollbar {
     display: none;
   }
@@ -17,6 +19,27 @@ const StyledReviewText = styled.div`
   font-size: 18px;
 `;
 
-const StyledReview = styled.div``;
+const StyledLink = styled.a`
+  text-decoration: none;
+  color: ${(props) => props.theme.colors.black};
+`;
 
-export { StyledReviewContainer, StyledReviewText, StyledReview };
+const StyledReview = styled.div`
+  padding: 3px;
+  border-radius: 10px;
+  border: 2px solid ${(props) => props.theme.colors.lightgray};
+  background-color: ${(props) => props.theme.colors.white};
+  font-family: ${(props) => props.theme.fonts.regularfont};
+  margin: 7px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${(props) => props.theme.colors.main};
+    color: ${(props) => props.theme.colors.white};
+  }
+`;
+
+export { StyledReviewContainer, StyledReviewText, StyledReview, StyledLink };
