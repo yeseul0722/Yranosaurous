8de6846cd70b7fb5b41c73de6e24209ad13f40ec
0214kbs <0214kbs@naver.com>
2023-10-05 02:24:43 +0900

    🎨 festival table 분리

---
+++ frontend/src/pagecomponents/expopage/components/festivaltable/index.tsx
@@ -0,0 +1,53 @@
+import { useEffect } from 'react';
+import {
+  StyledTable,
+  StyledTableHead,
+  CustomTableRow,
+  StyledTitle,
+  StyledContentContainer,
+  StyledContent,
+} from './Festivaltable.styled';
+import { useTodayFestivalHook } from '../../../../hooks/festival/useTodayFestivalHook';
+import useGuideStore from '../../../../stores/guide/useGuideStore';
+
+const FestivalTable = () => {
+  const { todayFestivalList, getTodayFestivalList, handleFestival } = useTodayFestivalHook();
+  const festivalID = useGuideStore((state: any) => state.festivalID);
+  useEffect(() => {
+    getTodayFestivalList();
+  }, []);
+  return (
+    <div>
+      <StyledTable>
+        <StyledTableHead>
+          <CustomTableRow>
+            <StyledTitle>시간</StyledTitle>
+            <StyledTitle>공연명</StyledTitle>
+            <StyledTitle>공연장소</StyledTitle>
+          </CustomTableRow>
+        </StyledTableHead>
+        <StyledContentContainer>
+          {todayFestivalList?.map((festival: any) => {
+            const dateTime = new Date(festival.startTime);
+            const timeString = dateTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+            return (
+              <CustomTableRow onClick={() => handleFestival(festival)} key={festival.id}>
+                <StyledContent festivalID={festivalID} id={festival.id}>
+                  {timeString}
+                </StyledContent>
+                <StyledContent festivalID={festivalID} id={festival.id}>
+                  {festival.name}
+                </StyledContent>
+                <StyledContent festivalID={festivalID} id={festival.id}>
+                  {festival.placeName}
+                </StyledContent>
+              </CustomTableRow>
+            );
+          })}
+        </StyledContentContainer>
+      </StyledTable>
+    </div>
+  );
+};
+
+export default FestivalTable;
