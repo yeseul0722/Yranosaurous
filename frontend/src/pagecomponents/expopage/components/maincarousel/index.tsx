823ace5ad544f6165e0dd43bada4bda595d2f100
신종혁 <shinjh4903@gmail.com>
2023-10-04 16:15:04 +0900

    💄 엑스포 메인 페이지 디자인 수정

--- frontend/src/pagecomponents/expopage/components/maincarousel/index.tsx
+++ frontend/src/pagecomponents/expopage/components/maincarousel/index.tsx
@@ -25,50 +25,48 @@ const MainCarouselComponent = () => {
   };
 
   return (
-    <div style={{ height: '100%' }}>
-      <StyledCarouselContainer>
-        <Slider {...settings}>
-          <StyledExpoMainPosterContainer image="1">
-            <StyledTextBox>
-              <StyledExpoMainPoster>
-                <StyledExpoInfo>아이들과</StyledExpoInfo>
-                <StyledExpoInfo>공룡세계엑스포부터</StyledExpoInfo>
-                <StyledExpoInfo>고성여행까지!</StyledExpoInfo>
-              </StyledExpoMainPoster>
-            </StyledTextBox>
-          </StyledExpoMainPosterContainer>
+    <StyledCarouselContainer>
+      <Slider {...settings}>
+        <StyledExpoMainPosterContainer image="1">
+          <StyledTextBox>
+            <StyledExpoMainPoster>
+              <StyledExpoInfo>아이들과</StyledExpoInfo>
+              <StyledExpoInfo>공룡세계엑스포부터</StyledExpoInfo>
+              <StyledExpoInfo>고성여행까지!</StyledExpoInfo>
+            </StyledExpoMainPoster>
+          </StyledTextBox>
+        </StyledExpoMainPosterContainer>
 
-          <StyledExpoMainPosterContainer image="2">
-            <StyledTextBox>
-              <StyledExpoMainPoster2>
-                <StyledExpoInfo>2023경남고성공룡엑스포</StyledExpoInfo>
-                <StyledExpoInfo2>Gyeongnam Goseong Dinosaur World Expo 2023</StyledExpoInfo2>
-                <StyledExpoInfo3>2023.09.11 ~ 2023.09.12(38일간)</StyledExpoInfo3>
-                <StyledExpoInfo3>입장시간 00:00 ~ 24:00</StyledExpoInfo3>
-              </StyledExpoMainPoster2>
-            </StyledTextBox>
-          </StyledExpoMainPosterContainer>
+        <StyledExpoMainPosterContainer image="2">
+          <StyledTextBox>
+            <StyledExpoMainPoster2>
+              <StyledExpoInfo>2023경남고성공룡엑스포</StyledExpoInfo>
+              <StyledExpoInfo2>Gyeongnam Goseong Dinosaur World Expo 2023</StyledExpoInfo2>
+              <StyledExpoInfo3>2023.09.11 ~ 2023.09.12(38일간)</StyledExpoInfo3>
+              <StyledExpoInfo3>입장시간 00:00 ~ 24:00</StyledExpoInfo3>
+            </StyledExpoMainPoster2>
+          </StyledTextBox>
+        </StyledExpoMainPosterContainer>
 
-          <StyledExpoMainPosterContainer image="3">
-            <StyledTextBox>
-              <StyledExpoMainPoster>
-                <StyledExpoInfo>관광도시 고성에서</StyledExpoInfo>
-                <StyledExpoInfo>아이들과 함께 즐겨보세요!</StyledExpoInfo>
-              </StyledExpoMainPoster>
-            </StyledTextBox>
-          </StyledExpoMainPosterContainer>
+        <StyledExpoMainPosterContainer image="3">
+          <StyledTextBox>
+            <StyledExpoMainPoster>
+              <StyledExpoInfo>관광도시 고성에서</StyledExpoInfo>
+              <StyledExpoInfo>아이들과 함께 즐겨보세요!</StyledExpoInfo>
+            </StyledExpoMainPoster>
+          </StyledTextBox>
+        </StyledExpoMainPosterContainer>
 
-          <StyledExpoMainPosterContainer image="4">
-            <StyledTextBox>
-              <StyledExpoMainPoster>
-                <StyledExpoInfo>공룡엑스포에 가기 전,</StyledExpoInfo>
-                <StyledExpoInfo>공룡에 대해 알아보고 갈까요?</StyledExpoInfo>
-              </StyledExpoMainPoster>
-            </StyledTextBox>
-          </StyledExpoMainPosterContainer>
-        </Slider>
-      </StyledCarouselContainer>
-    </div>
+        <StyledExpoMainPosterContainer image="4">
+          <StyledTextBox>
+            <StyledExpoMainPoster>
+              <StyledExpoInfo>공룡엑스포에 가기 전,</StyledExpoInfo>
+              <StyledExpoInfo>공룡에 대해 알아보고 갈까요?</StyledExpoInfo>
+            </StyledExpoMainPoster>
+          </StyledTextBox>
+        </StyledExpoMainPosterContainer>
+      </Slider>
+    </StyledCarouselContainer>
   );
 };
 
