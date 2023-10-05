8380e86480b5ae31733c7e0461e26aa927472197
alsuddlek1 <minyoung99311@gmail.com>
2023-10-05 23:24:41 +0900

    ✨ 랜더링 지도 크기 변경

--- frontend/src/pagecomponents/gosungpage/components/kakaomap/index.tsx
+++ frontend/src/pagecomponents/gosungpage/components/kakaomap/index.tsx
@@ -114,7 +114,7 @@ const GosungKakaoMapComponent = () => {
         width: '100%',
         height: '100vh',
       }}
-      level={3}
+      level={7}
     >
       {markers &&
         markers.map((marker, index) => (
