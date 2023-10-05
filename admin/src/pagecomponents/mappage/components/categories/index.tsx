15a197e507d3844ecad68959f3e2f0d51434d8f0
0214kbs <0214kbs@naver.com>
2023-10-05 16:58:59 +0900

    ✨ 실제 홈페이지로 가는 버튼 추가

--- admin/src/pagecomponents/mappage/components/categories/index.tsx
+++ admin/src/pagecomponents/mappage/components/categories/index.tsx
@@ -4,29 +4,37 @@ import { StyledCategories, StyledCategory } from './Categories.styled';
 const Categories = () => {
   const { selectcat, setselectcat } = useCategoryStore();
   return (
-    <StyledCategories>
-      <StyledCategory selected={selectcat === '1'} onClick={() => setselectcat('1')}>
-        <img src={`${process.env.REACT_APP_ADMIN_URL}/images/map/place.png`} style={{ width: '35px' }} />
-        <div>
-          <div>장소</div>
-          <div>등록</div>
-        </div>
-      </StyledCategory>
-      <StyledCategory selected={selectcat === '2'} onClick={() => setselectcat('2')}>
-        <img src={`${process.env.REACT_APP_ADMIN_URL}/images/map/festival.png`} style={{ width: '35px' }} />
-        <div>
-          <div>공연</div>
-          <div>등록</div>
-        </div>
-      </StyledCategory>
-      <StyledCategory selected={selectcat === '3'} onClick={() => setselectcat('3')}>
-        <img src={`${process.env.REACT_APP_ADMIN_URL}/images/map/course.png`} style={{ width: '40px' }} />
-        <div>
-          <div>코스</div>
-          <div>등록</div>
-        </div>
-      </StyledCategory>
-    </StyledCategories>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <StyledCategories>
+        <StyledCategory selected={selectcat === '1'} onClick={() => setselectcat('1')}>
+          <img src={`${process.env.REACT_APP_ADMIN_URL}/images/map/place.png`} style={{ width: '35px' }} />
+          <div>
+            <div>장소</div>
+            <div>등록</div>
+          </div>
+        </StyledCategory>
+        <StyledCategory selected={selectcat === '2'} onClick={() => setselectcat('2')}>
+          <img src={`${process.env.REACT_APP_ADMIN_URL}/images/map/festival.png`} style={{ width: '35px' }} />
+          <div>
+            <div>공연</div>
+            <div>등록</div>
+          </div>
+        </StyledCategory>
+        <StyledCategory selected={selectcat === '3'} onClick={() => setselectcat('3')}>
+          <img src={`${process.env.REACT_APP_ADMIN_URL}/images/map/course.png`} style={{ width: '40px' }} />
+          <div>
+            <div>코스</div>
+            <div>등록</div>
+          </div>
+        </StyledCategory>
+      </StyledCategories>
+      <a href="http://j9e102a.p.ssafy.io">
+        <img
+          src={`${process.env.REACT_APP_ADMIN_URL}/images/menu.png`}
+          style={{ width: '35px', marginBottom: '10px', cursor: 'pointer' }}
+        />
+      </a>
+    </div>
   );
 };
 
