5953e76ad4f17a5529ac029a750ffd4d50ba8cf8
0214kbs <0214kbs@naver.com>
2023-10-05 11:30:32 +0900

    💄 글자 크기 수정

--- frontend/src/pagecomponents/expopage/components/tourism/index.tsx
+++ frontend/src/pagecomponents/expopage/components/tourism/index.tsx
@@ -15,7 +15,7 @@ const Tourism = () => {
   return (
     <StyledTourismContainer>
       <StyledNavContainer>
-        <StyledText size="50px">고성에서 어디가지?</StyledText>
+        <StyledText size="43px">고성에서 어디가지?</StyledText>
         <StyledText size="24px">공룡엑스포만 보고 돌아가기 아쉬울 가족들을 위해 관광정보를 골라봤어요!</StyledText>
       </StyledNavContainer>
       <StyledCardContainer>
@@ -24,7 +24,7 @@ const Tourism = () => {
             <StyledCard src="expo_main_pic/restaurant.png" alt="restaurant" />
           </StyledCardBox>
           <StyledCardTextBox>
-            <StyledText size="32px">고성의 맛집</StyledText>
+            <StyledText size="30px">고성의 맛집</StyledText>
             <div>
               <StyledText size="20px">아이들과 함께 즐기기 좋은</StyledText>
               <StyledText size="20px">맛집, 유아시설, 주차 등</StyledText>
@@ -36,7 +36,7 @@ const Tourism = () => {
             <StyledCard src="expo_main_pic/tourism.png" alt="tourism" />
           </StyledCardBox>
           <StyledCardTextBox>
-            <StyledText size="32px">관광지</StyledText>
+            <StyledText size="30px">관광지</StyledText>
             <div>
               <StyledText size="20px">공룡엑스포만 보고 돌아가기 아쉬울 </StyledText>
               <StyledText size="20px">가족들을 위한 고성관광지 </StyledText>
@@ -48,7 +48,7 @@ const Tourism = () => {
             <StyledCard src="expo_main_pic/lodgment.png" alt="lodgment" />
           </StyledCardBox>
           <StyledCardTextBox>
-            <StyledText size="32px">숙박</StyledText>
+            <StyledText size="30px">숙박</StyledText>
             <div>
               <StyledText size="20px">아이들이랑 함께 자기 좋은 숙박시설!</StyledText>
               <StyledText size="20px">키즈 풀빌라, 청결 모두 챙겨보세요</StyledText>
